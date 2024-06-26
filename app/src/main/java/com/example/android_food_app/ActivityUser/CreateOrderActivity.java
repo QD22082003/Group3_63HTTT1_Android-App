1306fe8a2dddc24f336220344fa26b49fae0b78f
QD22082003 <buiquangdao2k3@gmail.com>
2024-06-26 16:17:27 +0700

    update order 26/6

--- app/src/main/java/com/example/android_food_app/ActivityUser/CreateOrderActivity.java
+++ app/src/main/java/com/example/android_food_app/ActivityUser/CreateOrderActivity.java
@@ -1,17 +1,23 @@
 package com.example.android_food_app.ActivityUser;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android_food_app.AdapterUser.OrderUserAdapter;
 import com.example.android_food_app.R;
 
 public class CreateOrderActivity extends AppCompatActivity {
-
+    TextView txtTotalAmount;
+    private RecyclerView rcvCreateOrder;
+    private OrderUserAdapter orderUserAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +28,15 @@ public class CreateOrderActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtTotalAmount = findViewById(R.id.txtTotalAmount);
+        rcvCreateOrder = findViewById(R.id.rcv_create_order);
+        // Get the total amount from the Intent
+        double totalAmount = getIntent().getDoubleExtra("TOTAL_AMOUNT", 0.0);
+        txtTotalAmount.setText(String.format("%.3f VND", totalAmount));
+
+        rcvCreateOrder.setLayoutManager(new LinearLayoutManager(this));
+        orderUserAdapter = new OrderUserAdapter(this);
+        rcvCreateOrder.setAdapter(orderUserAdapter);
     }
 }
