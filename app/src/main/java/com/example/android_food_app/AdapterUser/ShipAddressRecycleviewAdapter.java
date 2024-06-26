87e80ef6340342ba9435e252b3575c310e9748e2
dphuongha <dphuongha212003@gmail.com>
2024-06-26 17:35:21 +0700

    update BE 26/6

--- app/src/main/java/com/example/android_food_app/AdapterUser/ShipAddressRecycleviewAdapter.java
+++ app/src/main/java/com/example/android_food_app/AdapterUser/ShipAddressRecycleviewAdapter.java
@@ -1,5 +1,7 @@
 package com.example.android_food_app.AdapterUser;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android_food_app.ActivityUser.CreateOrderActivity;
+import com.example.android_food_app.ActivityUser.DetailPageUserActivity;
+import com.example.android_food_app.ActivityUser.ShipmentDetailPageActivity;
 import com.example.android_food_app.Model.Customer;
 import com.example.android_food_app.R;
 
@@ -18,10 +23,12 @@ import java.util.List;
 
 public class ShipAddressRecycleviewAdapter extends RecyclerView.Adapter<ShipAddressRecycleviewAdapter.ShipAddressViewHolder>{
     private List<Customer> listCustomer;
+    private Context mConText;
     private  IClickListener iClickListener;
     public interface IClickListener {
         void onClickUpdateItem(Customer customer);
         void onClickDeleteItem(Customer customer);
+        void onClick(Customer customer);
     }
 
     public ShipAddressRecycleviewAdapter(List<Customer> listCustomer, IClickListener iClickListener) {
@@ -47,6 +54,13 @@ public class ShipAddressRecycleviewAdapter extends RecyclerView.Adapter<ShipAddr
         holder.edit_text_name.setText("Họ và tên: " + customer.getName());
         holder.edit_text_phone.setText("Số điện thoại: " + customer.getPhone());
         holder.edit_text_address.setText("Địa chỉ: " + customer.getAddress());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iClickListener.onClick(customer);
+
+            }
+        });
         holder.btn_edit_address.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
